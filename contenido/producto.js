196c1ce30ea5db14c4202fa928e743ea403ab1ef
oliviavidela <ovidela@udesa.edu.ar>
2025-11-18 19:49:01 -0300

    chicas aca hice la del registro y producto, no estoy segura si anda pero cuando terminemos todo ahi probamos todo y corregimos

--- contenido/producto.js
+++ contenido/producto.js
@@ -1,17 +1,53 @@
-const params = new URLSearchParams(location.search);
-const id = params.get("id");
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+let idProducto = qsObj.get("id");
 
-fetch(https://dummyjson.com/products/${id})
-  .then(res => res.json())
-  .then(prod => {
-    document.querySelector(".descripcion").innerHTML = `
-      <h1>${prod.title}</h1>
-      <p><b>Marca:</b> ${prod.brand}</p>
-      <p><b>Categoría:</b> ${prod.category}</p>
-      <p>${prod.description}</p>
-      <p><b>Precio:</b> $${prod.price}</p>
-      <p><b>Stock:</b> ${prod.stock}</p>
-      <p><i>#${prod.tags ? prod.tags.join(" #") : ""}</i></p>
-    `;
-    document.querySelector(".foto_producto").src = prod.thumbnail;
-  });
+let contenedorInfo = document.querySelector(".info");
+let contenedorResenas = document.querySelector(".reseñas");
+
+contenedorInfo.innerHTML = "";
+contenedorResenas.innerHTML = "<h3>Resenas de usuarios</h3>";
+
+fetch("https://dummyjson.com/products/" + idProducto)
+    .then(function(respuesta){
+        return respuesta.json();
+    })
+    .then(function(producto){
+        contenedorInfo.innerHTML = `
+            <img src="${producto.thumbnail}" class="foto_producto">
+                
+                <div class="descripcion">
+                    <h1>${producto.title}</h1>
+                    <p><b>Marca:</b> ${producto.brand}</p>
+                    <p><b>Categoria:</b>
+                        <a href="./category.html?cat=${producto.category}">${producto.category}</a></p> 
+                    <p>${producto.description}</p>
+                    <p><b>Precio:</b>$${producto.price}</p>
+                    <p><b>Stock:</b> ${producto.stock}</p>
+                    <p><i>#${producto.tags[0] || ""} #${producto.tags[1] || ""} #${producto.tags[2] || ""}</i></p>  
+                    </div>
+                `;
+            return fetch("https://dummyjson.com/products/" + idProducto + "/reviews");
+        })
+        .then(function(respuesta){
+            return respuesta.json();
+        })
+       
+        .then(function(datosReviews){
+            let lista = datosReviews.reviews;
+
+            for(let i = 0; i < lista.length; i++){
+                let r = lista[i];
+
+                contenedorResenas.innerHTML += `
+                <p> 
+                    ⭐Rating: ${r.rating}<br>
+                    "${r.comment}" <br>
+                    <i>- ${r.reviewerName}, ${r.date}</i>
+                </p>
+            `;
+            }
+        })
+        .catch(function(error){
+            console.log("Error:", error);
+        });
